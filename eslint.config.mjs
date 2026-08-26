692ff69309d472851f163dd60ee9fcbaa203a1e4
Sparsh Mishra <callme8samay@gmail.com>
2026-08-26 05:55:22 +0000

    chore: refresh dependencies and lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default [...nextVitals];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default compat.extends("next/core-web-vitals");
